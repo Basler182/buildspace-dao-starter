799d273a2141fb5e48c14c5658919ce695d8775c
Basler182 <showdialog@web.de>
2022-01-02 21:21:36 +0100

    feat claim condition

---
+++ scripts/4-set-claim-condition.js
@@ -0,0 +1,23 @@
+import sdk from "./1-initialize-sdk.js";
+
+const bundleDrop = sdk.getBundleDropModule(
+  "0x86B026C3B60d983ee98E0d40Ee41C9BcB635E55C",
+);
+
+(async () => {
+  try {
+    const claimConditionFactory = bundleDrop.getClaimConditionFactory();
+    // Specify conditions.
+    claimConditionFactory.newClaimPhase({
+      startTime: new Date(),
+      maxQuantity: 50_000,
+      maxQuantityPerTransaction: 1,
+    });
+    
+    
+    await bundleDrop.setClaimCondition(0, claimConditionFactory);
+    console.log("✅ Successfully set claim condition on bundle drop:", bundleDrop.address);
+  } catch (error) {
+    console.error("Failed to set claim condition", error);
+  }
+})()
