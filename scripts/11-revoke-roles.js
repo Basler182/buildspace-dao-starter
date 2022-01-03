dbe4344131fcf371fae17ae8c7726332b13f9c3e
Basler182 <showdialog@web.de>
2022-01-03 21:48:56 +0100

    feat revoke roles

---
+++ scripts/11-revoke-roles.js
@@ -0,0 +1,26 @@
+import sdk from "./1-initialize-sdk.js";
+
+const tokenModule = sdk.getTokenModule(
+  "0x45923a3E0b40bFc3ae16bff9c52999B0F38b5d77",
+);
+
+(async () => {
+  try {
+    // Log the current roles.
+    console.log(
+      "👀 Roles that exist right now:",
+      await tokenModule.getAllRoleMembers()
+    );
+
+    // Revoke all the superpowers your wallet had over the ERC-20 contract.
+    await tokenModule.revokeAllRolesFromAddress(process.env.WALLET_ADDRESS);
+    console.log(
+      "🎉 Roles after revoking ourselves",
+      await tokenModule.getAllRoleMembers()
+    );
+    console.log("✅ Successfully revoked our superpowers from the ERC-20 contract");
+
+  } catch (error) {
+    console.error("Failed to revoke ourselves from the DAO treasury", error);
+  }
+})();
